b99cdb5418acc61e5e499ae38015bcbb69f77e77
andre.luiznascimento <andre.luiznascimento@senior.com.br>
2017-03-27 14:23:00 -0300

    small fix

--- tests/Example_test.js
+++ tests/Example_test.js
@@ -4,7 +4,7 @@ Feature('Testing Example Feature');
 Scenario('test something', (I) => {
     I.amOnPage('https://www.w3schools.com/html/html_iframe.asp');
     I.seeInTitle('Iframes');
-    I.amOnPage('https://www.w3schools.com/html/html_iframe.asp').within('a[href*=\'#1\']', function(){
+    I.amOnPage('https://www.w3schools.com/html/html_iframe.asp').within('a[href*=\'html_quiz.asp\']', function(){
         I.click('Start HTML Quiz!')
     },['iframe[src*=\default.asp\'']);
     ;    
